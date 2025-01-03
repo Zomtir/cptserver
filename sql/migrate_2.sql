fc199da3861b961f3b94cf677a5cd7642f5bcfe1
Moritz Oberhauser <moritz.oberhauser@gmail.com>
2025-01-03 18:54:26 +0100

    fix: Missing default values of organisation rights

--- sql/migrate_2.sql
+++ sql/migrate_2.sql
@@ -3,3 +3,7 @@ UPDATE _info SET version = 2;
 
 -- Increase nickname length
 ALTER TABLE `users` CHANGE `nickname` `nickname` VARCHAR(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+
+-- Add missing default values to organisation rights
+ALTER TABLE `teams` CHANGE `right_organisation_read` `right_organisation_read` TINYINT(1) NOT NULL DEFAULT '0';
+ALTER TABLE `teams` CHANGE `right_organisation_write` `right_organisation_write` TINYINT(1) NOT NULL DEFAULT '0';
