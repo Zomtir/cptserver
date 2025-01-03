a01a452b3d33ae036ae57e432e67da5c74eb2794
Moritz Oberhauser <moritz.oberhauser@gmail.com>
2025-01-03 18:46:49 +0100

    feat: Increased nickname length from 20 to 40

--- sql/migrate_2.sql
+++ sql/migrate_2.sql
@@ -1,2 +1,5 @@
 -- Update version
 UPDATE _info SET version = 2;
+
+-- Increase nickname length
+ALTER TABLE `users` CHANGE `nickname` `nickname` VARCHAR(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
