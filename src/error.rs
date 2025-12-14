0da38276c3833d7af31db869d71deff07eb435d7
Moritz Oberhauser <moritz.oberhauser@gmail.com>
2025-12-14 21:31:26 +0100

    refactor: Removed dead anyhow code

--- src/error.rs
+++ src/error.rs
@@ -180,21 +180,3 @@ impl ErrorKind {
 }
 
 pub type Result<T = ()> = std::result::Result<T, ErrorKind>;
-
-#[derive(Debug)]
-pub struct Error(pub anyhow::Error);
-
-impl<E> From<E> for Error
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(error: E) -> Self {
-        Error(error.into())
-    }
-}
-
-impl<'r> Responder<'r, 'static> for Error {
-    fn respond_to(self, request: &Request<'_>) -> response::Result<'static> {
-        response::Debug(self.0).respond_to(request)
-    }
-}
